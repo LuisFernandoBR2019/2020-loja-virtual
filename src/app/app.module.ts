8fd62ebd53ac4840dd636d5c684fe5b68cfcffa5
Luis Fernando <nando_srs@hotmail.com>
2020-05-27 21:22:47 -0300

    alterações Luciano

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {RouterModule } from '@angular/router'
-
-import {ROUTES} from './app.routes'
-
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    NotFoundComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-
-    RouterModule.forRoot(ROUTES, {})
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
