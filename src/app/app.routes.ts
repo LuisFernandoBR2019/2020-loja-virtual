0add28a8483e9a76113b2a27a85039283105a8bf
Luis Fernando <nando_srs@hotmail.com>
2020-05-27 23:14:52 -0300

    Merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-<<<<<<< HEAD
-
-export const ROUTES: Routes = [
-    { path:'', component: HomeComponent},
-    { path:'about', component: AboutComponent},
-    {path: '**', component: NotFoundComponent}
-]
-
-=======
 import { ProductListComponent } from './product/product-list/product-list.component';
 
 
@@ -22,4 +13,3 @@ export const ROUTES: Routes = [
     { path: '**', component: NotFoundComponent },
     
 ]
->>>>>>> 6df1c91cda0b69b1a62a9bb1ab3b9d64d710539c
